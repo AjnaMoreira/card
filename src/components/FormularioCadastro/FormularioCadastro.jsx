4398e80c020af5c01f5fe50d33c45bd71cb75ce1
Ajna Moreira <ajna.moreira@gmail.com>
2022-02-04 16:43:29 -0300

    first commit

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import './style.css'
+
+class FormularioCadastro extends Component {
+    constructor(props) {
+        super(props)
+        this.titulo = ''
+        this.texto = ''
+    }
+
+    #handleMudancaTitulo(event) {
+        event.stopPropagation()
+        this.titulo = event.target.value
+    }
+
+    #handleMudancaTexto(event) {
+        event.stopPropagation()
+        this.texto = event.target.value
+    }
+
+    #criarNota(event) {
+        event.preventDefault()
+        event.stopPropagation()
+        this.props.criarNota(this.titulo, this.texto)
+    }
+
+    render() {
+        return (
+            <form
+                className="form-cadastro"
+                onSubmit={this.#criarNota.bind(this)}
+            >
+                <input
+                    type="text"
+                    placeholder="Título"
+                    className="form-cadastro_input"
+                    onChange={this.#handleMudancaTitulo.bind(this)}
+                />
+                <textarea
+                    rows={15}
+                    placeholder="Escreva sua nota"
+                    className="form-cadastro_input"
+                    onChange={this.#handleMudancaTexto.bind(this)}
+                />
+                <button className="form-cadastro_input form-cadastro_submit">
+                    Criar Nota
+                </button>
+            </form>
+        )
+    }
+}
+
+export default FormularioCadastro
